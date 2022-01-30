5c1346b5bb2c9c84677b5f48a501ae31c8f498ef
Sushrut1101 <guptasushrut@gmail.com>
2022-01-30 20:20:24 +0530

    Initial Commit
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Make the Directory if it doesn't exist
+mkdir -p $SYNC_PATH
+
+# Change to the Source Directory
+cd $SYNC_PATH
+
+# Init Repo
+repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
+
+# Sync the Sources
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
+
+# Clone Trees
+git clone --single-branch --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+git clone --single-branch --depth=1 $DCT_LINK $DCT_PATH
+git clone --single-branch --depth=1 $VT_LINK $VT_PATH
+git clone --single-branch --depth=1 $VCT_LINK $VCT_PATH
+git clone --single-branch --depth=1 $KT_LINK $KT_PATH
+
+# Exit
+exit 0
---
+++ vars.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Manifest
+export MANIFEST=""
+export MANIFEST_BRANCH=""
+
+# Device Info
+export DEVICE="" # Codename
+export OEM=""
+export PLATFORM="" # eg: sm6250, mt6765 etc.
+
+# Trees
+export DT_LINK=""
+export DCT_LINK="" # Device Common Tree, Leave Blank if None
+export VT_LINK=""
+export VCT_LINK="" # Vendor Common Tree, Leave Blank if None
+export KT_LINK="" # Kernel Tree, Leave Blank if Prebuilt
+
+# Trees Path
+export DT_PATH="device/$OEM/$DEVICE"
+export DCT_PATH="device/$OEM/${PLATFORM}-common"
+export VT_PATH="vendor/$OEM/$DEVICE"
+export VCT_PATH="vendor/$OEM/${PLATFORM}-common"
+export KT_PATH="kernel/$OEM/$PLATFORM"
+
+# Lunch Combos and Targets
+export USE_BRUNCH_AS_TARGET=false # set it to "true" if you want to use brunch
+export LUNCH_COMBO="" # eg: "aosp_miatoll-userdebug", No need if USE_BRUNCH_AS_TARGET is set to true
+export TARGET="" # eg: "bacon", No need if USE_BRUNCH_AS_TARGET is set to true
+
+# Output File (to upload)
+export OUTPUT_FILE="" # eg: "PixelExperience*.zip"
+
+# Extra Command
+export EXTRA_CMD=""
+
+# Not Recommended to Change
+export SYNC_PATH=$HOME/work
+export USE_CCACHE=1
+export CCACHE_EXEC=$(which ccache)
+export CCACHE_DIR="${SYNC_PATH}/.ccache"
+export CCACHE_SIZE=30G
+export J_VAL=16 # for -jx, eg: "16"
