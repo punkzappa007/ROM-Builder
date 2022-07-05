8bdeff7ac634950d3a6f5062069f1b6edf757321
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-07-05 10:19:00 +0800

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Manifest
-export MANIFEST=""
-export MANIFEST_BRANCH=""
+export MANIFEST="https://github.com/LineageOS/android.git"
+export MANIFEST_BRANCH="lineage-19.1"
 
 # Device Info
-export DEVICE="" # Codename
-export OEM=""
-export PLATFORM="" # eg: sm6250, mt6765 etc.
+export DEVICE="TECNO-CG8" # Codename
+export OEM="TECNO"
+export PLATFORM="MT6785" # eg: sm6250, mt6765 etc.
 
 # Trees
-export DT_LINK=""
+export DT_LINK="https://github.com/punkzappa007/android_device_tecno_cg8.git"
 export DCT_LINK="" # Device Common Tree, Leave Blank if None
 export VT_LINK=""
 export VCT_LINK="" # Vendor Common Tree, Leave Blank if None
@@ -25,11 +25,11 @@ export KT_PATH="kernel/$OEM/$PLATFORM"
 
 # Lunch Combos and Targets
 export USE_BRUNCH_AS_TARGET=false # set it to "true" if you want to use brunch
-export LUNCH_COMBO="" # eg: "aosp_miatoll-userdebug", No need if USE_BRUNCH_AS_TARGET is set to true
-export TARGET="" # eg: "bacon", No need if USE_BRUNCH_AS_TARGET is set to true
+export LUNCH_COMBO="lineage_TECNO-CG8-userdebug" # eg: "aosp_miatoll-userdebug", No need if USE_BRUNCH_AS_TARGET is set to true
+export TARGET="bacon" # eg: "bacon", No need if USE_BRUNCH_AS_TARGET is set to true
 
 # Output File (to upload)
-export OUTPUT_FILE="" # eg: "PixelExperience*.zip"
+export OUTPUT_FILE="Lineage.zip" # eg: "PixelExperience*.zip"
 
 # Extra Command
 export EXTRA_CMD=""
