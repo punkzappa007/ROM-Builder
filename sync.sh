65f3947dcf10fa5a68c5d003b87fe1caa7bf770f
Tata Lee <67614323+punkzappa007@users.noreply.github.com>
2022-07-05 11:06:50 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -13,11 +13,11 @@ repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-19.1
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle
 
 # Clone Trees
-git clone --single-branch --depth=1 $DT_LINK $DT_PATH || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
-git clone --single-branch --depth=1 $DCT_LINK $DCT_PATH
-git clone --single-branch --depth=1 $VT_LINK $VT_PATH
-git clone --single-branch --depth=1 $VCT_LINK $VCT_PATH
-git clone --single-branch --depth=1 $KT_LINK $KT_PATH
+git clone --single-branch --depth=1 https://github.com/punkzappa007/android_device_tecno_cg8.git device/tecno/TECNO-CG8 || { echo "ERROR: Failed to Clone the Device Trees!" && exit 1; }
+#git clone --single-branch --depth=1 $DCT_LINK $DCT_PATH
+#git clone --single-branch --depth=1 $VT_LINK $VT_PATH
+#git clone --single-branch --depth=1 $VCT_LINK $VCT_PATH
+#git clone --single-branch --depth=1 $KT_LINK $KT_PATH
 
 # Exit
 exit 0
